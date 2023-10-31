964cd89c049e6389238392de39ed9b1e9961a7fd
Vic <thangson62@gmail.com>
2023-10-31 21:43:08 +0700

    Repair
    Frm_HanhKhach: thay thế textbox bằng numericUpDown

--- GUIs/Frm_HanhKhach.designer.cs
+++ GUIs/Frm_HanhKhach.designer.cs
@@ -30,14 +30,16 @@
         {
             this.Btn_Close = new System.Windows.Forms.Button();
             this.Pnl_TreEm = new System.Windows.Forms.Panel();
+            this.Lbl_TreEm = new System.Windows.Forms.Label();
             this.Pnl_NguoiLon = new System.Windows.Forms.Panel();
             this.Lbl_NguoiLon = new System.Windows.Forms.Label();
-            this.Lbl_TreEm = new System.Windows.Forms.Label();
-            this.Txt_NguoiLon = new System.Windows.Forms.TextBox();
-            this.Txt_TreEm = new System.Windows.Forms.TextBox();
             this.Btn_Chon = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
             this.Pnl_TreEm.SuspendLayout();
             this.Pnl_NguoiLon.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
             this.SuspendLayout();
             // 
             // Btn_Close
@@ -55,7 +57,7 @@
             // 
             // Pnl_TreEm
             // 
-            this.Pnl_TreEm.Controls.Add(this.Txt_TreEm);
+            this.Pnl_TreEm.Controls.Add(this.numericUpDown2);
             this.Pnl_TreEm.Controls.Add(this.Lbl_TreEm);
             this.Pnl_TreEm.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Pnl_TreEm.Location = new System.Drawing.Point(2, 134);
@@ -63,9 +65,18 @@
             this.Pnl_TreEm.Size = new System.Drawing.Size(360, 89);
             this.Pnl_TreEm.TabIndex = 2;
             // 
+            // Lbl_TreEm
+            // 
+            this.Lbl_TreEm.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lbl_TreEm.Location = new System.Drawing.Point(32, 34);
+            this.Lbl_TreEm.Name = "Lbl_TreEm";
+            this.Lbl_TreEm.Size = new System.Drawing.Size(75, 28);
+            this.Lbl_TreEm.TabIndex = 1;
+            this.Lbl_TreEm.Text = "Trẻ em";
+            // 
             // Pnl_NguoiLon
             // 
-            this.Pnl_NguoiLon.Controls.Add(this.Txt_NguoiLon);
+            this.Pnl_NguoiLon.Controls.Add(this.numericUpDown1);
             this.Pnl_NguoiLon.Controls.Add(this.Lbl_NguoiLon);
             this.Pnl_NguoiLon.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Pnl_NguoiLon.Location = new System.Drawing.Point(2, 39);
@@ -78,32 +89,9 @@
             this.Lbl_NguoiLon.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Lbl_NguoiLon.Location = new System.Drawing.Point(32, 34);
             this.Lbl_NguoiLon.Name = "Lbl_NguoiLon";
-            this.Lbl_NguoiLon.Size = new System.Drawing.Size(98, 21);
+            this.Lbl_NguoiLon.Size = new System.Drawing.Size(98, 28);
             this.Lbl_NguoiLon.TabIndex = 0;
             this.Lbl_NguoiLon.Text = "Người lớn";
-            // 
-            // Lbl_TreEm
-            // 
-            this.Lbl_TreEm.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Lbl_TreEm.Location = new System.Drawing.Point(32, 34);
-            this.Lbl_TreEm.Name = "Lbl_TreEm";
-            this.Lbl_TreEm.Size = new System.Drawing.Size(75, 21);
-            this.Lbl_TreEm.TabIndex = 1;
-            this.Lbl_TreEm.Text = "Trẻ em";
-            // 
-            // Txt_NguoiLon
-            // 
-            this.Txt_NguoiLon.Location = new System.Drawing.Point(178, 31);
-            this.Txt_NguoiLon.Name = "Txt_NguoiLon";
-            this.Txt_NguoiLon.Size = new System.Drawing.Size(105, 30);
-            this.Txt_NguoiLon.TabIndex = 1;
-            // 
-            // Txt_TreEm
-            // 
-            this.Txt_TreEm.Location = new System.Drawing.Point(178, 31);
-            this.Txt_TreEm.Name = "Txt_TreEm";
-            this.Txt_TreEm.Size = new System.Drawing.Size(105, 30);
-            this.Txt_TreEm.TabIndex = 2;
             // 
             // Btn_Chon
             // 
@@ -114,6 +102,40 @@
             this.Btn_Chon.TabIndex = 3;
             this.Btn_Chon.Text = "Chọn";
             this.Btn_Chon.UseVisualStyleBackColor = true;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(178, 32);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 30);
+            this.numericUpDown1.TabIndex = 2;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(178, 32);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(120, 30);
+            this.numericUpDown2.TabIndex = 2;
             // 
             // Frm_HanhKhach
             // 
@@ -130,9 +152,9 @@
             this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
             this.Text = "Hành Khách";
             this.Pnl_TreEm.ResumeLayout(false);
-            this.Pnl_TreEm.PerformLayout();
             this.Pnl_NguoiLon.ResumeLayout(false);
-            this.Pnl_NguoiLon.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -144,8 +166,8 @@
         private System.Windows.Forms.Panel Pnl_NguoiLon;
         private System.Windows.Forms.Label Lbl_NguoiLon;
         private System.Windows.Forms.Label Lbl_TreEm;
-        private System.Windows.Forms.TextBox Txt_TreEm;
-        private System.Windows.Forms.TextBox Txt_NguoiLon;
         private System.Windows.Forms.Button Btn_Chon;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
     }
 }
