fb4e0055e622786abdc82491b8796fe639356102
Tiến Lê <ww.hacker01@gmail.com>
2023-11-01 20:04:30 +0700

    Function
    Thêm chức năng chuyển hướng sang Frm_TimKiemChuyenBay sau khi bấm vào chuyến bay được đề xuất

--- Program.cs
+++ Program.cs
@@ -14,7 +14,7 @@ namespace QuanLyBanVeMayBay
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Frm_MuaVe1());
+            Application.Run(new Frm_TrangChuNguoiDung());
         }
     }
 }
